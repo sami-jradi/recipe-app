91a68f8460e7c60cc2ac58c60a6b203bb0dc5c62
z00450vb <sami.jradi@siemens.com>
2020-09-14 10:43:15 +0200

    Initial commit

---
+++ src/test/java/sami/springframework/RecipeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package sami.springframework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
