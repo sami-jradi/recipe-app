3668163a7ee1cc8de6c20862daab1545a58a47dd
z00450vb <sami.jradi@siemens.com>
2020-09-16 10:02:13 +0200

    adds bidirectional relationships between Recipe, Notes and Ingredient

--- src/main/java/sami/springframework/domain/Ingredient.java
+++ src/main/java/sami/springframework/domain/Ingredient.java
@@ -22,12 +22,17 @@ public class Ingredient {
     public Ingredient() {
     }
 
+    public Ingredient(String description, BigDecimal amount, UnitOfMeasure uom) {
+        this.description = description;
+        this.amount = amount;
+        this.uom = uom;
+    }
+
     public Ingredient(String description, BigDecimal amount, UnitOfMeasure uom, Recipe recipe) {
         this.description = description;
         this.amount = amount;
         this.uom = uom;
         this.recipe = recipe;
-
     }
 
     public Long getId() {
